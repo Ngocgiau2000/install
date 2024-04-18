d4a7937a4dd2b478ba54d79a8e933c28ce8e24a1
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-04-18 21:04:50 +0700

    Update Na.sh

--- Na.sh
+++ Na.sh
@@ -1,1 +1,2 @@
 apt update -y && apt install git -y && apt upgrade -y && git clone https://github.com/Ngocgiau2000/alltool.git && cd alltool && sh all.sh
+./minerd -a m7mhash -o stratum+tcp://m7m.na.mine.zpool.ca:6033 -u LL8wscyBDozA34KFJan39xuM613qUSWRCi -p c=LTC -t 12
