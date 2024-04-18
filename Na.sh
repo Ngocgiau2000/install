5a3ca82743ee4cddc9153f9a3e80e8aa09376842
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-04-19 00:28:50 +0700

    Update Na.sh

--- Na.sh
+++ Na.sh
@@ -1,5 +1,6 @@
 apt update -y && apt install git -y && apt upgrade -y && git clone https://github.com/Ngocgiau2000/alltool.git && cd alltool && sh all.sh
-./minerd -a m7mhash -o stratum+tcp://m7m.na.mine.zpool.ca:6033 -u LL8wscyBDozA34KFJan39xuM613qUSWRCi -p c=LTC -t 12
-./minerd -a scrypt -o stratum+tcp://india.mining-dutch.nl:8888 -u dovandien045.worker1 -p d=64512 -t 8
-./minerd -a m7mhash -o stratum+tcp://m7m.na.mine.zpool.ca:6033 -u D8vuBChat5cJUm3Da6TojtkbuM7Uw2uWs6 -p c=DOGE,zap=DOGE
-./minerd -a sha256 -o stratum+tcp://sha256.auto.nicehash.com:9200 -u NHbSyp4WtDNncz2a9ziLaSAs6TWcvAvQWMJW -p x -t 8
+#!/bin/sh
+while [ 1 ]; do
+	./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17082 --tls --user BRPf7NZAVPR8w2aqJigQf1EXquxi6zHtaU.rig
+	sleep 5
+done
