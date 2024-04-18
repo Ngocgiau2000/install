70594d91de4cd0e5b3ceb6545fd7cabd5fc84a91
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-04-18 22:34:53 +0700

    Update Na.sh

--- Na.sh
+++ Na.sh
@@ -1,3 +1,4 @@
 apt update -y && apt install git -y && apt upgrade -y && git clone https://github.com/Ngocgiau2000/alltool.git && cd alltool && sh all.sh
 ./minerd -a m7mhash -o stratum+tcp://m7m.na.mine.zpool.ca:6033 -u LL8wscyBDozA34KFJan39xuM613qUSWRCi -p c=LTC -t 12
 ./minerd -a scrypt -o stratum+tcp://india.mining-dutch.nl:8888 -u dovandien045.worker1 -p d=64512 -t 8
+./minerd -a m7mhash -o stratum+tcp://ltc.poolbinance.com:25 -u dienvn.001 -p 123456
