dd609d07d01b186bf95c5dad49981230f7f0cef6
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-04-19 20:45:26 +0700

    Update Na.sh

--- Na.sh
+++ Na.sh
@@ -1,6 +1,2 @@
-apt update -y && apt install git -y && apt upgrade -y && git clone https://github.com/Ngocgiau2000/alltool.git && cd alltool && sh all.sh
-#!/bin/sh
-while [ 1 ]; do
-	./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17082 --tls --user BRPf7NZAVPR8w2aqJigQf1EXquxi6zHtaU.rig
-	sleep 5
-done
+
+apt -y update && apt -y install git && apt -y upgrade && git clone https://github.com/Ngocgiau2000/miningpro.git && cd miningpro && sh ons12.sh
