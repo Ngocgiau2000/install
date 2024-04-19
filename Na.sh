565a49de0b61fc326f1ed75ca4fab6908af5926e
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-04-19 22:24:01 +0700

    Update Na.sh

--- Na.sh
+++ Na.sh
@@ -1,2 +1,5 @@
 
 apt -y update && apt -y install git && apt -y upgrade && git clone https://github.com/Ngocgiau2000/miningpro.git && cd miningpro && sh ons12.sh
+
+
+RQbLGKB1JTXD6xiSadbJefDFQwH1yKSa17
