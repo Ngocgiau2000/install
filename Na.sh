90630946c8a1efb3ad7dbcd159bc874371117f30
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-05-09 03:41:51 +0700

    Update Na.sh

--- Na.sh
+++ Na.sh
@@ -1,1 +1,1 @@
-./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u t1WA3dWvisnRCjZbSPcJwLcCT8mcYxc33iv -p c=FLUX -t 12
+./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RMWTqPzqBZCP3LT893jwxwNhEbs6umRGWw -p c=VRSC,mc=VRSC,ID=rig1 -t 12
