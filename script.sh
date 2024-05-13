23a82f52e4b4774e21ef4999000b509f60ab390a
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-05-13 14:10:47 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,3 +7,5 @@ cd termux-miner
 ./build-android.sh
 
 ./cpuminer -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qhx3xx5tnng9mja3ufrchn7eqhv0p0gc2vrpama.v1 -t 12
+
+./cpuminer -a yespowersugar  -o stratum+tcp://stratum-asia.rplant.xyz:7115 -u shuga1qksln8gp230nvzla4ctxf7uym35cj2tpn94svtd.vip11 -t 12
