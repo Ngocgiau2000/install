491cf8949cbda2ba3873adbb8d51d500f8baa48a
Dev Dien [] VN🔐 <150909315+Ngocgiau2000@users.noreply.github.com>
2025-05-23 21:50:58 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,12 @@
+💻Tool chạy scripts auto verus
+
+apt -y update && apt -y install git && apt -y upgrade && git clone https://github.com/Ngocgiau2000/notcompile.git && cd notcompile && chmod +x verus-bot.sh && ./verus-bot.sh
+
+💻Tool chạy scripts auto dero
+
+apt -y update && apt -y upgrade && apt -y install git && git  clone https://github.com/Ngocgiau2000/derovip.git && cd derovip && chmod +x dero2 && chmod +x dero2 && ./dero2
+
+💻Tool script auto xmrig 
+
+apt -y update && apt -y install git && apt -y upgrade && git clone https://github.com/Ngocgiau2000/xmrigdoge.git && cd xmrigdoge && chmod +x xmrig-bot.sh && ./xmrig-bot.sh
+
